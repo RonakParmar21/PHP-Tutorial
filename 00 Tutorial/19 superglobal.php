ff72c801feb04193a2225c38b14155ba23481190
Ronak Parmar <parmarronak21112003@gmail.com>
2025-03-20 14:01:10 +0530

    changes folder name

---
+++ 00 Tutorial/19 superglobal.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>superglobal in PHP</title>
+</head>
+<body>
+    <h1>superglobal in PHP</h1>
+</body>
+</html>
