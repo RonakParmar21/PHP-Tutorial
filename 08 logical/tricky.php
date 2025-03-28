866dfcd961ffacaae12b69b2891d2d12943a6bf4
Ronak Parmar <parmarronak21112003@gmail.com>
2025-03-28 08:26:13 +0530

    some tricky program

---
+++ 08 logical/tricky.php
@@ -0,0 +1,20 @@
+<?php 
+    // $a = 5;
+    // $b = ++$a;
+    // echo $b."<br/>";
+    // $c = $b--;
+    // echo $c."<br/>";
+    // $a = ++$b;
+    // echo $a."<br/>";
+    // $d = $a + ++$a + $a++;
+    // echo "<br/>";
+    // echo $a."<br/>";
+    // echo $b."<br/>";
+    // echo $c."<br/>";
+    // echo $d."<br/>";
+
+    // $str = 500?"Hello":"Bye";
+    // echo $str;
+
+    
+?>
